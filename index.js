d00c91949b4fa46f6c248e103da266ab804b57e3
Michael DuBois <michael@michaeldubois.me>
2016-12-18 15:29:02 -0800

    add interactiveSpring option

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var clamp = require('clamp')
 var defined = require('defined')
 
+var sign = function(x) {
+  x = +x; // convert to a number
+  if (x === 0 || isNaN(x)) {
+    return Number(x);
+  }
+  return x > 0 ? 1 : -1;
+}
+
 module.exports = createSpringInput
 function createSpringInput (opt) {
   return new SpringInput(opt)
@@ -19,15 +27,33 @@ function SpringInput (opt) {
   this.max = defined(opt.max, 1)
   this.edge = opt.edge || 0
   this.damping = defined(opt.damping, 0.3)
-  this.spring = defined(opt.spring, 0.2)
   this.maxVelocity = defined(opt.maxVelocity, 0.01)
+
+  this.spring = defined(opt.spring, 0.2)
+
+  // Default to zeros for backwards compatibility
+  this.interactingSpring = defined(opt.interactingSpring, 0)
+  this.tweenToInteractingMs = defined(opt.tweenToInteractingMs, 0)
+  this.tweenToFreeMs = defined(opt.tweenToFreeMs, 0)
+
+  this.activeSpring = this.spring
 }
 
 SpringInput.prototype.update = function () {
   var isBefore = this.value < this.min
   var isAfter = this.value > this.max
-  var dipping = !this.interacting
   var dip = 0
+
+  // Ease between springs
+  var tweenMs = this.interacting ? this.tweenToInteractingMs : this.tweenToFreeMs
+  var numTweenSteps = Math.max(1, tweenMs / 16) // assume 16 fps
+  var springTweenStep = Math.abs(this.spring - this.interactingSpring) / numTweenSteps
+  var targetSpring = this.interacting ? this.interactingSpring : this.spring
+  var targetDirection = sign(targetSpring - this.activeSpring)
+  if (this.activeSpring !== targetSpring) {
+    var tweenedSpring = this.activeSpring + targetDirection * springTweenStep
+    this.activeSpring = clamp(tweenedSpring, 0, targetSpring)
+  }
 
   // ease input at edges
   if (isBefore) {
@@ -43,14 +69,12 @@ SpringInput.prototype.update = function () {
   }
 
   // dip back to edge
-  if (dipping) {
-    if (isBefore) {
-      dip = this.value - this.min
-    } else if (isAfter) {
-      dip = this.value - this.max
-    }
-    dip *= this.spring
+  if (isBefore) {
+    dip = this.value - this.min
+  } else if (isAfter) {
+    dip = this.value - this.max
   }
+  dip *= this.activeSpring
 
   // integrate
   this.value += this.inputDelta
@@ -60,6 +84,16 @@ SpringInput.prototype.update = function () {
   }
   this.velocity *= 1 - this.damping
   this.value -= dip
+  this.value = clamp(this.value, this.min - this.edge, this.max + this.edge)
+
+  this.value += (this.interacting ? 0 : this.velocity) + this.inputDelta - dip
+  // 1. Don't cannibalize the user input velocity
+  // 2. Only apply damping when the velocity is opposed to the spring force
+  if (!this.interacting && dip !== 0 && sign(this.velocity) !== sign(-1 * dip)) {
+    this.velocity *= 1 - this.damping
+  }
+
+  this.inputDelta = 0
   this.value = clamp(this.value, this.min - this.edge, this.max + this.edge)
 }
 
